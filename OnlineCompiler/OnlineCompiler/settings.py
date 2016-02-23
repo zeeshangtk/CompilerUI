a5a452cbdb497b6e4120943408cfad0e3eaf4a7c
zeeshana <zeeshana@thougthworks.com>
2016-02-24 00:19:32 +0530

    Configured to run test

--- OnlineCompiler/OnlineCompiler/settings.py
+++ OnlineCompiler/OnlineCompiler/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'online_compiler',
-        'USER': 'zeeshana',
-        'PASSWORD': 'namchi123',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
